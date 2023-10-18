38bdb0010742c972342332b58981893bbabb4c99
Dunn <ducpt@vmodev.com>
2023-10-18 18:58:04 +0700

    new

---
+++ lib/Home.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 40, 10, 50),
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 0, 50, 0),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black, width: 3),
+                ),
+                height: 70,
+                width: double.infinity,
+                child: Center(
+                  child: Row(
+                    children: const [
+                      Icon(Icons.add_road),
+                      SizedBox(width: 10),
+                      Text('Quãng đường đi'),
+                      Spacer(),
+                      Text('30,00 km'),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: -10,
+                left: 30,
+                child: Container(
+                  height: 20,
+                  width: 70,
+                  color: Colors.white,
+                  child: const Center(
+                    child: Text(
+                      'Tháng 10',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(10, 0, 10, 30),
+          height: 500,
+          child: SingleChildScrollView(
+            child: Column(
+              children: const [
+                BoxItem(),
+                BoxItem(),
+                BoxItem(),
+                BoxItem(),
+                BoxItem(),
+                BoxItem(),
+                BoxItem(),
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class BoxItem extends StatelessWidget {
+  const BoxItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 1.5),
+      ),
+      height: 100,
+      width: double.infinity,
+      child: Row(
+        children: [
+          const Icon(Icons.directions_walk_outlined, size: 45),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: const [
+                Text('10-10'),
+                Spacer(),
+                Text('Công viên Hòa Bình'),
+                Spacer(),
+                Text('1 giờ 15 phút'),
+              ],
+            ),
+          ),
+          const Spacer(),
+          const Icon(Icons.navigate_next),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+import 'Home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Hiker Management App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    HomeScreen(),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            backgroundColor: Colors.black,
+            icon: Icon(Icons.home),
+            label: 'Trang chủ',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.black,
+            icon: Icon(Icons.person),
+            label: 'Tài khoản',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.black,
+            icon: Icon(Icons.add_circle),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.black,
+            icon: Icon(Icons.list_alt),
+            label: 'Báo cáo',
+          ),
+          BottomNavigationBarItem(
+            backgroundColor: Colors.black,
+            icon: Icon(Icons.list),
+            label: 'Khác',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
